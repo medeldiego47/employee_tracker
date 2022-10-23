03a5fd711f8f3edd2d082388b9d01887c847ffcc
DiegoM <medeldiego47@yahoo.com>
2022-10-23 13:36:58 -0500

    established sql connection in server js file

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
-const inquirer = require(inquirer);
-const sql2 = require(mysql2);
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
 const consoleTable= require('console.table');
+
+const connection = mysql.createConnection( {
+    host:"localhost",
+    user:"root",
+    password: process.env.PASSWORD,
+    database:"employee_db"
+});
+connection.connect (function(err){
+    (err)? console.log(err):console.log(`connected as ${connection.threadId}`)
+})
