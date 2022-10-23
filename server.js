3f2e5d47d4fca3de60434762ec7856a8d3e01cf6
DiegoM <medeldiego47@yahoo.com>
2022-10-23 13:44:00 -0500

    added inquirer prompt for menu

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable= require('console.table');
 
+
 const connection = mysql.createConnection( {
     host:"localhost",
     user:"root",
@@ -10,5 +11,26 @@ const connection = mysql.createConnection( {
     database:"employee_db"
 });
 connection.connect (function(err){
-    (err)? console.log(err):console.log(`connected as ${connection.threadId}`)
-})
+    (err)? console.log(err):console.log(`connected as ${connection.threadId}`);
+    
+    init()
+});
+
+function init(){
+    inquirer.prompt({
+        type:"list",
+        choices:[
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employees Role",
+            "Exit"
+
+        ],
+        message:"Please select an option from the list below",
+        name:"menu"
+    })
+}
